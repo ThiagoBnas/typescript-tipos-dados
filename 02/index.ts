487ec7050be29d91989b1a80e8646d18d51078ea
ThiagoBnas <thiagobreves.nascimento@gmail.com>
2023-12-21 15:49:05 -0400

    exercícios 1 e 2

---
+++ 02/index.ts
@@ -0,0 +1,42 @@
+const usuarios: { nome: string; idade: number; status: boolean }[] = [
+	{
+		nome: "Guido",
+		idade: 32,
+		status: true,
+	},
+	{
+		nome: "Dani",
+		idade: 30,
+		status: true,
+	},
+	{
+		nome: "João",
+		idade: 40,
+		status: false,
+	},
+	{
+		nome: "Guilherme",
+		idade: 29,
+		status: true,
+	},
+	{
+		nome: "Ana",
+		idade: 18,
+		status: false,
+	},
+	{
+		nome: "José",
+		idade: 28,
+		status: false,
+	},
+];
+
+const filtroProNome = (lista: {nome: string; idade: number; status: boolean}[],	nomeUsuario: string): { nome: string; idade: number; status: boolean }[] => {
+let colecao:{nome: string; idade: number; status: boolean}[] = lista.filter(elemento=>{
+	return elemento.nome.includes(nomeUsuario);
+});
+
+return colecao;
+}
+
+console.log(filtroProNome(usuarios,'Jo'))
